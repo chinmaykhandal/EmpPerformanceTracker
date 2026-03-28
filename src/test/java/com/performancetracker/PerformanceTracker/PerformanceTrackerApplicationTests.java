2e5e019fb7331172cb80adf861310c1e30c0817d
Chinmay Khandal <khandalchinmay@gmail.com>
2026-03-28 13:02:37 +0530

    project setup

---
+++ src/test/java/com/performancetracker/PerformanceTracker/PerformanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performancetracker.PerformanceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
